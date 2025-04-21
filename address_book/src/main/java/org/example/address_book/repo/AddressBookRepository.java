07fccde3597da03e0eebc827ab7567acfa35c45f
vanshika <vanshika2260@chitkara.edu.in>
2025-04-21 23:52:42 +0530

    [Vanshika] Add. address book app

---
+++ address_book/src/main/java/org/example/address_book/repo/AddressBookRepository.java
@@ -0,0 +1,10 @@
+package org.example.address_book.repo;
+
+
+import org.example.address_book.model.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressBookRepository extends JpaRepository<AddressBook, Integer> {
+}
